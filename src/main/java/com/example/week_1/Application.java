f30ed6e1c906f288cdc4c4ab2f4540cf40747a7b
tmsksfh2012 <tmsksfh2012@naver.com>
2023-07-13 20:41:44 +0900

    first commit

---
+++ src/main/java/com/example/week_1/Application.java
@@ -0,0 +1,37 @@
+package com.example.week_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+public class Application {
+
+    public static void main(String[] args) {
+        boolean isTrue = true;
+        int number = 1234;
+        double realNumber = 3.14;
+        String str = "Hello world";
+
+        System.out.println(isTrue);
+        System.out.println(number);
+        System.out.println(realNumber);
+        System.out.println(str);
+
+        int grade = 100;
+
+        if(grade >= 90) {
+            System.out.println("A");
+        }
+        else if(grade >= 80) {
+            System.out.println("B");
+        }
+        else if(grade >= 70) {
+            System.out.println("C");
+        }
+        else if(grade >= 60) {
+            System.out.println("D");
+        }
+        else {
+            System.out.println("F");
+        }
+    }
+}
